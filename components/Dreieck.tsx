d56d7c6cca9ef5717a69ce9fb27e0b7e10ac9695
Philipp Friedrich <freezer05@googlemail.com>
2022-08-30 18:58:22 +0200

    small redesign for fast iteration

--- components/Dreieck.tsx
+++ components/Dreieck.tsx
@@ -8,11 +8,6 @@ function Dreieck({}: Props) {
   const [messageText, setMessageText] = useState("");
   return (
     <div className="">
-      <div className="dreieck">
-        <div className="p-4 bg-gradient-to-b from-cyan-500 to-blue-500"></div>
-        <div className="p-4 bg-gradient-to-b from-blue-500 to-orange-500"></div>
-        <div className="p-4 bg-gradient-to-b from-orange-500 to-emerald-500"></div>
-      </div>
       <form
         className="flex p-2 gap-4 text"
         onSubmit={(e) => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,24 +5,26 @@ import Dreieck from "../components/Dreieck";
 
 const Home: NextPage = () => {
   return (
-    <div className="grid gap-8 h-full w-full justify-center items-end p-12 border relative">
-      <div className="flex items-end rainbowParent h-full w-full absolute">
+    <div className="h-screen relative">
+      <div className="h-full w-full absolute">
         <div className="blueBox bg-blue-500 absolute h-full w-1/2 right-0 bg-opacity-50"></div>
         <div className="orangeBox bg-orange-500 absolute h-full w-1/2 left-0 bg-opacity-50"></div>
         <div className="redTriangle bg-red-500 absolute h-full w-1/2 bg-opacity-50"></div>
         <div className="yellowTriangle bg-yellow-500 absolute h-full w-1/2 bg-opacity-50"></div>
         <div className="greenTriangle bg-green-500 absolute h-full w-1/2 right-0 bg-opacity-50"></div>
         <div className="purpleTriangle bg-purple-500 absolute h-full w-1/2 right-0 bg-opacity-50"></div>
-      </div>
-      <div className="grid gap-8 justify-center items-center p-12">
-        <div>
-          <div className="">
+        <div className="absolute h-full w-full">
+          <div className="flex justify-center h-full items-center">
             <Messages />
           </div>
         </div>
-        <div>
-          <div className="p-8 z-10">
-            <Dreieck></Dreieck>
+      </div>
+      <div className="absolute bottom-0 w-full  bg-gradient-to-b from-transparent to-gray-800">
+        <div className="flex justify-center">
+          <div>
+            <div className="py-2 z-10">
+              <Dreieck></Dreieck>
+            </div>
           </div>
         </div>
       </div>
