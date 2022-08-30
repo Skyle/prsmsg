af5e4a5214ffbf4980dc655442e23ea487242392
Marion Schloer <marions.22589@gmail.com>
2022-08-30 15:34:06 +0200

    deleted shoot

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,9 +8,7 @@ function Button({}: Props) {
       <button
         type="submit"
         className="border-b-green-600 border-b-[40px] border-r-transparent border-r-[25px] border-l-transparent border-l-[25px] hover:border-b-pink-600"
-      >
-        shoot
-      </button>
+      ></button>
     </div>
   );
 }
