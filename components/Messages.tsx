5687bbaefe8b4cdbf06e006fa39c75003d76db4b
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-08-31 16:17:23 +0200

    add bubble style

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -26,5 +26,28 @@ export const Messages = () => {
     handleSubscription,
   );
 
-  return <div>{JSON.stringify(res.data)}</div>;
+  return (
+    <div>
+      <div>
+        {res.data?.map((message) => {
+          return (
+            <div className="w-[300px] h-[100px] rounded-3xl  ml-100px mt-100px relative bg-white pt-6 m-10 text-black text-center translate-y-68 duration-500 transition-all">
+              {message.text}
+              <div
+                className=" px-2 py-3 m-1 -top-8 -left-4 bg-white text-black w-[120px] absolute rounded-full"
+                key={message.id}
+              >
+                {/* {new Date(message.createdAt!).getDate()}.
+                {new Date(message.createdAt!).getMonth() + 1}.
+                {new Date(message.createdAt!).getFullYear()} */}
+                {new Date(message.createdAt!).getHours()}:
+                {new Date(message.createdAt!).getMinutes()}:
+                {new Date(message.createdAt!).getSeconds()}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
