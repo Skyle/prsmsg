32d7bb8d2399f78a2e7e16fdffc412061f15756d
zuehler <jakob.diedrich@gmx.de>
2022-08-31 12:52:16 +0200

    changed transparency in msg-board background

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,12 @@ const Home: NextPage = () => {
   return (
     <div className="h-screen relative">
       <div className="h-full w-full absolute">
-        <div className="blueBox bg-blue-500 absolute h-full w-1/2 right-0 bg-opacity-50"></div>
-        <div className="orangeBox bg-orange-500 absolute h-full w-1/2 left-0 bg-opacity-50"></div>
-        <div className="redTriangle bg-red-500 absolute h-full w-1/2 bg-opacity-50"></div>
-        <div className="yellowTriangle bg-yellow-500 absolute h-full w-1/2 bg-opacity-50"></div>
-        <div className="greenTriangle bg-green-500 absolute h-full w-1/2 right-0 bg-opacity-50"></div>
-        <div className="purpleTriangle bg-purple-500 absolute h-full w-1/2 right-0 bg-opacity-50"></div>
+        <div className="blueBox bg-gradient-to-tr from-transparent to-blue-500 absolute h-full w-1/2 right-0"></div>
+        <div className="orangeBox bg-gradient-to-tl from-transparent to-orange-500 absolute h-full w-1/2 left-0"></div>
+        <div className="redTriangle bg-gradient-to-l from-transparent to-red-500 absolute h-full w-1/2 bg-opacity-50"></div>
+        <div className="yellowTriangle bg-gradient-to-t from-transparent to-yellow-500 absolute h-full w-1/2"></div>
+        <div className="greenTriangle bg-gradient-to-t from-transparent to-green-500 absolute h-full w-1/2 right-0"></div>
+        <div className="purpleTriangle bg-gradient-to-r from-transparent to-purple-500 absolute h-full w-1/2 right-0"></div>
         <div className="absolute h-full w-full">
           <div className="flex justify-center h-full items-center">
             <Messages />
